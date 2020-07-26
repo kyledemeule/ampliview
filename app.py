25e90aee8470a7175ebd6e02ab3441ca584766ef
Kyle Demeule <kyle.demeule@gmail.com>
2020-07-25 21:06:56 -0700

    Better sentence tokenization and rendering.

--- app.py
+++ app.py
@@ -3,11 +3,28 @@ from flask import Flask, request, jsonify, render_template
 from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
 app = Flask(__name__)
 
+# for tokenizing reviews
+import nltk
+import nltk.tokenize.punkt as pkt
+nltk.download('punkt')
+class CustomLanguageVars(pkt.PunktLanguageVars):
+    _period_context_fmt = r"""
+        \S*                          # some word material
+        %(SentEndChars)s             # a potential sentence ending
+        \s*                       #  <-- THIS is what I changed
+        (?=(?P<after_tok>
+            %(NonWord)s              # either other punctuation
+            |
+            (?P<next_tok>\S+)     #  <-- Normally you would have \s+ here
+        ))"""
+sentence_tokenizer = pkt.PunktSentenceTokenizer(lang_vars=CustomLanguageVars())
+
 @app.route('/analyze', methods=['POST'])
 def analyze():
     review_text = request.form.get('review_text')
     if review_text:
-        review_lines = [process_line(l) for l in review_text.split(".")]
+        review_lines = [process_line(l) for l in sentence_tokenizer.tokenize(review_text)]
+        print(review_lines)
         return jsonify({
             "review_html": render_template('review.html', review_lines=review_lines),
             "helpful_prediction": 0.93
